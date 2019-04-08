1c106bbe2b2b04b9359580b293d7b635f8996460
pradeepasakthi <protopradeepa@gmail.com>
2019-04-08 11:24:21 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+router.get('/',function(req,res) {
+    res.status(200);
+    res.send("Hello World");
+});
+
+app.use('/',router);
+
+app.listen(3000);
